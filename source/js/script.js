1602eb75e75efc91464ae4f07c3d669c52c2d6b1
Alexander Drach <alexander.drach.adci@gmail.com>
2023-09-23 15:27:38 +0600

    header  mobile first, mobile menu js

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,7 @@
-/* в этот файл добавляет скрипты*/
+/* mobile menu */
+const humb = document.querySelector('.humb');
+const header = document.querySelector('.page-header');
+
+humb.addEventListener('click', () => {
+  header.classList.toggle('open-menu');
+})
