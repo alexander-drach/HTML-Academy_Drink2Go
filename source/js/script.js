fd4f2172cf01ff9ed1f9dae6378c8f317d1fa923
Alexander Drach <alexander.drach.adci@gmail.com>
2023-09-24 14:01:19 +0600

    add slider, add slider images

--- source/js/script.js
+++ source/js/script.js
@@ -4,4 +4,22 @@ const header = document.querySelector('.page-header');
 
 humb.addEventListener('click', () => {
   header.classList.toggle('open-menu');
-})
+});
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
